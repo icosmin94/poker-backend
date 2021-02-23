be8470bb385d94a50663b0b53e728679093ebf40
Cosmin George Iacob <George.Iacob@orange.com>
2021-02-23 22:27:29 +0200

    add card deck management micro service

---
+++ deck_management/src/main/java/com/project/poker/deck_management/DeckManagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.poker.deck_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeckManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeckManagementApplication.class, args);
+    }
+
+}
---
+++ deck_management/src/test/java/com/project/poker/deck_management/DeckManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.poker.deck_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeckManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
