82b873487e55fb64e30e334ec4e4bc17d992e997
Cosmin George Iacob <George.Iacob@orange.com>
2021-03-02 00:33:48 +0200

    create micro services databases

--- dependencies/postgres_init_scripts/test_db.sh
+++ dependencies/postgres_init_scripts/test_db.sh
@@ -5,4 +5,12 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE USER keycloak PASSWORD 'keycloak';
     CREATE DATABASE keycloak;
     GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
+
+    CREATE USER user_management PASSWORD 'user_management';
+    CREATE DATABASE user_management;
+    GRANT ALL PRIVILEGES ON DATABASE user_management TO user_management;
+
+    CREATE USER deck_management PASSWORD 'deck_management';
+    CREATE DATABASE deck_management;
+    GRANT ALL PRIVILEGES ON DATABASE deck_management TO deck_management;
 EOSQL
