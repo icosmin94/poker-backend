b04b651e21c797bf01fb219793b0a81b118fc868
Cosmin George Iacob <George.Iacob@orange.com>
2021-04-12 00:10:26 +0300

    remove 1 from poker cards

--- common-lib/src/main/java/com/project/poker/commonlib/model/CardNumber.java
+++ common-lib/src/main/java/com/project/poker/commonlib/model/CardNumber.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum CardNumber {
-    Ace("A"), One("1"), Two("2"), Three("3"),
+    Ace("A"), Two("2"), Three("3"),
     Four("4"), Five("5"), Six("6"), Seven("7"),
     Eight("8"), Nine("9"), Ten("10"), Jack("J"),
     Queen("Q"), King("K");
