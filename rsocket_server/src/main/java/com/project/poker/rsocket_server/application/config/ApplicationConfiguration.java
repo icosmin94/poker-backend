8f97da50e25db5d96e25837e9c194c21d2786e1d
Cosmin George Iacob <George.Iacob@orange.com>
2021-04-14 00:58:44 +0300

    load balance rsocket requests modify rsocket not to start extra netty server fool eureka to register correct rsocket port

--- rsocket_server/src/main/java/com/project/poker/rsocket_server/application/config/ApplicationConfiguration.java
+++ rsocket_server/src/main/java/com/project/poker/rsocket_server/application/config/ApplicationConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(CommonConfiguration.class)
 public class ApplicationConfiguration {
 
 }
